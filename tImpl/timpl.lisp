518a22a26d198b6df5205dc3be24e3816d708f9f
Tyler Combs <rylerdevteam@gmail.com>
2013-11-10 16:14:15 -0600

    all work to generate a tree for the next state complete

---
+++ tImpl/timpl.lisp
@@ -0,0 +1,96 @@
+(IN-PACKAGE "ACL2")
+(include-book "io-utilities" :dir :teachpacks)
+(include-book "avl-rational-keys" :dir :teachpacks)
+(include-book "list-utilities" :dir :teachpacks)
+(set-state-ok t)
+
+
+(defun num-list->tree(xs)
+  (if (consp xs)
+      (avl-insert (num-list->tree (cdr xs)) (car xs) nil)
+      (empty-tree)))
+
+(defun chrs->num-list(cs acc)
+  (if (consp cs)
+      (if (equal #\NewLine (car cs))
+          (cons (str->rat (chrs->str acc)) 
+                (chrs->num-list (cdr cs) nil))
+          (chrs->num-list (cdr cs) (append acc (list (car cs)))))
+      (if (null acc) nil
+          (cons (str->rat (chrs->str acc)) nil)))) ;if something in accumulator, convert to number
+
+(defun file->chr-list (file-path state)
+  (mv-let (str error state)
+          (file->string file-path state)
+     (if error
+         (mv error state)
+         (str->chrs str))))
+
+(defun coord->key(x y w)
+  (+ (* y w) x))
+(defun get-neighbor-keys(x y w h)
+  (list
+   (coord->key (mod (- x 1) w) (mod (- y 1) h) w)
+   (coord->key (mod (- x 1) w) y w)
+   (coord->key (mod (- x 1) w) (mod (+ y 1) h) w)
+   (coord->key x (mod (+ y 1) h) w)
+   (coord->key (mod (+ x 1) w) (mod (+ y 1) h) w)
+   (coord->key (mod (+ x 1) w) y w)
+   (coord->key (mod (+ x 1) w) (mod (- y 1) h) w)
+   (coord->key x (mod (- y 1) h) w)
+   ;i = y
+   ;j = x
+   ;(i,j)
+   ;(i-1 mod h, j-1 mod w)
+   ;(i, j-1 mod w)
+   ;(i+1 mod h, j-1 mod w)
+   ;(i+1 mod h, j)
+   ;(i+1 mod h, j+1 mod w)
+   ;(i, j+1 mod w)
+   ;(i-1 mod w, j+1 mod w)
+   ;(i-1 mod h, j)
+   ))
+
+(defun number-alive(keys tree) 
+  (if (consp keys)
+      (if (avl-retrieve tree (car keys))
+          (+ 1 (number-alive (cdr keys) tree))
+          (number-alive (cdr keys) tree))
+      0))
+
+;for the two tests, t -> cell should be alive f-> cell should be dead
+(defun alive-test(num-alive)
+  (or (= num-alive 2) (= num-alive 3)))
+
+(defun dead-test(num-alive)
+  (= num-alive 3))
+
+(defun next-state-tree(w h x y old-tree new-tree)
+  (if (= x w)
+      (if (= y h)
+          new-tree ;end of matrix
+          (next-state-tree w h 0 (+ y 1) old-tree new-tree)) ;end of row, start new row
+      (let* (
+             (me (coord->key x y w))
+             (im-alive (avl-retrieve old-tree me))
+             (neighbors (get-neighbor-keys x y w h))
+             (num-neighbors-alive (number-alive neighbors old-tree))
+             ) 
+        (if im-alive
+            (if (alive-test num-neighbors-alive)
+                (next-state-tree w h (+ x 1) y old-tree (avl-insert new-tree me nil))
+                (next-state-tree w h (+ x 1) y old-tree new-tree))
+            (if (dead-test num-neighbors-alive)
+                (next-state-tree w h (+ x 1) y old-tree (avl-insert new-tree me nil))
+                (next-state-tree w h (+ x 1) y old-tree new-tree))))))
+
+(defun main(state)
+  (let* (
+         (old-num-list (chrs->num-list(file->chr-list "game-state" state) nil))
+         (w (car old-num-list))
+         (h (car (cdr old-num-list)))
+         (old-tree (num-list->tree(cdr (cdr old-num-list))))
+         )
+     old-tree))
+
+
